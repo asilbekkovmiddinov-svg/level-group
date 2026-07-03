1887fe2f0bae1aa18aafa9fc8ab5e1295d01eb28
asilbekkovmiddinov-svg <asilbekkovmiddinov@gmail.com>
2026-07-03 18:48:14 +0300

    Update wallet.py

--- backend/app/routers/wallet.py
+++ backend/app/routers/wallet.py
@@ -1,1 +1,31 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
+from app.core.database import get_db
+from app.crud.wallet import get_wallet
+
+router = APIRouter(
+    prefix="/wallet",
+    tags=["Wallet"]
+)
+
+
+@router.get("/{telegram_id}")
+def wallet_info(
+    telegram_id: int,
+    db: Session = Depends(get_db)
+):
+    wallet = get_wallet(db, telegram_id)
+
+    if not wallet:
+        return {
+            "message": "Wallet not found"
+        }
+
+    return {
+        "telegram_id": wallet.telegram_id,
+        "efc_balance": float(wallet.efc_balance),
+        "uzs_balance": float(wallet.uzs_balance),
+        "locked_efc": float(wallet.locked_efc),
+        "locked_uzs": float(wallet.locked_uzs)
+    }
