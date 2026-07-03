7b20de0ea63336cb2e9495febf12ce231aa569f0
asilbekkovmiddinov-svg <asilbekkovmiddinov@gmail.com>
2026-07-03 15:01:31 +0300

    Update database.py

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -22,6 +22,7 @@ def get_db():
         yield db
     finally:
         db.close()
-        
+
+
 def create_tables():
-Base.metadata.create_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
