1cc24a3a1a84a55afc5200adfc2e541ae3ec805c
asilbekkovmiddinov-svg <asilbekkovmiddinov@gmail.com>
2026-07-03 14:22:53 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
+from app.routers.auth import router as auth_router
 
 app = FastAPI(
     title="LEVEL_GROUP API",
     version="1.0.0"
 )
+
+app.include_router(auth_router)
 
 
 @app.get("/")
