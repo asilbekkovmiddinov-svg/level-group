27d0d44305520b18c11cdc1694bfbef19ce2b7dc
asilbekkovmiddinov-svg <asilbekkovmiddinov@gmail.com>
2026-07-03 09:35:32 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,12 @@
-# LEVEL_GROUP Backend
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="LEVEL_GROUP API",
+    version="1.0.0"
+)
+
+@app.get("/")
+def home():
+    return {
+        "message": "LEVEL_GROUP Backend is running"
+    }
