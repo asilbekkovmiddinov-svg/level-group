dab3af2300cd33dc9f95f0c80d6118a7433d762a
asilbekkovmiddinov-svg <asilbekkovmiddinov@gmail.com>
2026-07-03 18:28:49 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 
+from app.core.database import create_tables
+import app.models
+
 from app.routers.auth import router as auth_router
 from app.routers.user import router as user_router
 
@@ -7,6 +10,8 @@ app = FastAPI(
     title="LEVEL_GROUP API",
     version="1.0.0"
 )
+
+create_tables()
 
 app.include_router(auth_router)
 app.include_router(user_router)
