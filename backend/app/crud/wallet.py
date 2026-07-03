8587f717ebd7ca97246d2b00499ab14b8ac0ffec
asilbekkovmiddinov-svg <asilbekkovmiddinov@gmail.com>
2026-07-03 15:26:59 +0300

    Update wallet.py

--- backend/app/crud/wallet.py
+++ backend/app/crud/wallet.py
@@ -1,1 +1,25 @@
+from sqlalchemy.orm import Session
 
+from app.models.wallet import Wallet
+
+
+def get_wallet(db: Session, telegram_id: int):
+    return db.query(Wallet).filter(
+        Wallet.telegram_id == telegram_id
+    ).first()
+
+
+def create_wallet(db: Session, telegram_id: int):
+    wallet = Wallet(
+        telegram_id=telegram_id,
+        efc_balance=0,
+        uzs_balance=0,
+        locked_efc=0,
+        locked_uzs=0
+    )
+
+    db.add(wallet)
+    db.commit()
+    db.refresh(wallet)
+
+    return wallet
