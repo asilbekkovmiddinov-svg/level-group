cbc0630c597f8cc1b9844a504f32ae569b7da280
asilbekkovmiddinov-svg <asilbekkovmiddinov@gmail.com>
2026-07-03 19:43:10 +0300

    Update wallet.py

--- backend/app/schemas/wallet.py
+++ backend/app/schemas/wallet.py
@@ -1,12 +1,6 @@
 from pydantic import BaseModel
 
 
-class WalletResponse(BaseModel):
+class AddEFC(BaseModel):
     telegram_id: int
-    efc_balance: float
-    uzs_balance: float
-    locked_efc: float
-    locked_uzs: float
-
-    class Config:
-        from_attributes = True
+    amount: float
