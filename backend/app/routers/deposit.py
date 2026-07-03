b396d54e8b6f2d8e6a952b2197b0e3fd99b3226b
asilbekkovmiddinov-svg <asilbekkovmiddinov@gmail.com>
2026-07-03 20:40:38 +0300

    Update deposit.py

--- backend/app/routers/deposit.py
+++ backend/app/routers/deposit.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from app.core.database import get_db
-from app.crud.deposit import create_deposit, get_deposits
+from app.crud.deposit import create_deposit, get_deposits, approve_deposit
 from app.schemas.deposit import DepositCreate
 
 router = APIRouter(
@@ -30,3 +30,24 @@ def create_deposit_request(
 @router.get("/all")
 def all_deposits(db: Session = Depends(get_db)):
     return get_deposits(db)
+
+
+@router.post("/approve/{deposit_id}")
+def approve_deposit_request(
+    deposit_id: int,
+    admin_id: int,
+    db: Session = Depends(get_db)
+):
+    deposit = approve_deposit(db, deposit_id, admin_id)
+
+    if not deposit:
+        return {
+            "message": "Deposit not found"
+        }
+
+    return {
+        "message": "Deposit approved",
+        "deposit_id": deposit.id,
+        "status": deposit.status,
+        "approved_by": deposit.approved_by
+    }
