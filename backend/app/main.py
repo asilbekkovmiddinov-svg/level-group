bb337feb8546c1fe583089fb48ffd29d66535a32
asilbekkovmiddinov-svg <asilbekkovmiddinov@gmail.com>
2026-07-05 20:18:35 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,6 +12,7 @@ from app.routers.deposit import router as deposit_router
 from app.routers.withdraw import router as withdraw_router
 from app.routers.product import router as product_router
 from app.routers.order import router as order_router
+from app.routers.p2p import router as p2p_router
 from app.routers.system import router as system_router
 
 
@@ -31,6 +32,7 @@ app.include_router(deposit_router)
 app.include_router(withdraw_router)
 app.include_router(product_router)
 app.include_router(order_router)
+app.include_router(p2p_router)
 app.include_router(system_router)
 
 
