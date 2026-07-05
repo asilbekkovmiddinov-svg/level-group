d176721b42059253ba63c3087601bf98836b0377
asilbekkovmiddinov-svg <asilbekkovmiddinov@gmail.com>
2026-07-05 10:02:39 +0300

    Update withdraw.py

--- backend/app/schemas/withdraw.py
+++ backend/app/schemas/withdraw.py
@@ -4,3 +4,12 @@ from pydantic import BaseModel
 class WithdrawCreate(BaseModel):
     telegram_id: int
     amount: float
+
+
+class WithdrawAdminAction(BaseModel):
+    admin_id: int
+
+
+class WithdrawReject(BaseModel):
+    admin_id: int
+    reason: str
