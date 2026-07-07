968ff83d9db9bde3e10a327d17dcfede9fdbe2ad
asilbekkovmiddinov-svg <asilbekkovmiddinov@gmail.com>
2026-07-07 10:15:46 +0300

    Update user.py

--- backend/app/crud/user.py
+++ backend/app/crud/user.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timezone
+
 from sqlalchemy.orm import Session
 
 from app.models.user import User
@@ -15,7 +17,8 @@ def create_user(db: Session, user: UserCreate):
         telegram_id=user.telegram_id,
         first_name=user.first_name,
         username=user.username,
-        language=user.language
+        language=user.language,
+        last_seen_at=datetime.now(timezone.utc),
     )
 
     db.add(db_user)
@@ -23,3 +26,17 @@ def create_user(db: Session, user: UserCreate):
     db.refresh(db_user)
 
     return db_user
+
+
+def update_user_last_seen(db: Session, telegram_id: int):
+    user = get_user(db=db, telegram_id=telegram_id)
+
+    if not user:
+        return None
+
+    user.last_seen_at = datetime.now(timezone.utc)
+
+    db.commit()
+    db.refresh(user)
+
+    return user
